b7553ea641982944f736f5c93f25a303b5748091
pbasswerner <paula.bass.werner@gmail.com>
2021-08-13 04:21:10 -0400

    connection to clue and pison device

--- app/src/main/java/com/example/plantier/Application.kt
+++ app/src/main/java/com/example/plantier/Application.kt
@@ -1,13 +1,16 @@
 package com.example.plantier
 
 import android.app.Application
+import com.juul.kable.Scanner
 import com.pison.core.client.PisonRemoteServer
 import com.pison.core.client.bindToLocalServer
 import com.pison.core.client.newPisonSdkInstance
+import kotlinx.coroutines.flow.first
 
 class Application : Application() {
     companion object {
         lateinit var sdk: PisonRemoteServer
+//        lateinit var clueDevice:
     }
 
     override fun onCreate() {
@@ -15,4 +18,11 @@ class Application : Application() {
         sdk = newPisonSdkInstance(applicationContext).bindToLocalServer()
 
     }
+
+//
+//    val advertisement = Scanner()
+//        .advertisements
+//        .first { it.name?.startsWith("Example") }
+//
+//    val clue = scope.peripheral(advertisement)
 }
